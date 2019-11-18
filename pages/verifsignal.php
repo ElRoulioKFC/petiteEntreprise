bf1237bdd93fb51ced3bbcf5a87369dce2ae04b6
brunelle <brunelle.cordier--pierre-bes@orange.fr>
2019-11-18 13:27:59 +0100

    verification du formulaire de signalement
    en cours

--- pages/verifsignal.php
+++ pages/verifsignal.php
@@ -1,1 +1,23 @@
-verifsignal.php
+<?php 
+session_start();
+$errors = [];
+
+if (!isset($_POST["description"]) ||
+    $_POST["description"] == "")
+    $errors[] = "description";
+	
+	
+
+
+if (count($errors) > 0) {
+	$_SESSION["errors"] = $errors;
+	$_SESSION["old"] = $_POST;
+	header("Location: index.php");
+} else {
+	header("Location: welcome.php");
+}
+
+
+
+
+?>
