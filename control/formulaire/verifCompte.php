4fb731cc80a0cae9c0bcd1d369d3ff2ad08d0207
damien <damien16.05.1999@hotmail.fr>
2019-12-10 09:49:37 +0100

    deco reco

--- control/formulaire/verifCompte.php
+++ control/formulaire/verifCompte.php
@@ -1,7 +1,16 @@
 <?php
+    $sql = "SELECT * FROM membre";
+	$results = mysqli_query($c,$sql);
 
-    //bdd
-    setcookie($_SESSION['cookie_user'], $_POST['nom']);
-    header('Location: ./index.php');
+	// Récupération des résultats
+
+	while($row = mysqli_fetch_assoc($results)) {
+		$nom = $row["name"];
+		$mdp = $row["motDePasse"];
+
+		if($nom == $_POST["nom"] and $mdp == $_POST["mdp"]){
+	    	setcookie($_SESSION['cookie_user'], $_POST['nom']);
+    		header('Location: ./index.php');
+		}
 	}
 ?>
--- view/page/nouvelUtilisateur.php
+++ view/page/nouvelUtilisateur.php
@@ -9,7 +9,7 @@
         <tr><td>Votre site (uniquement pour association): </td><td><input type="text" name="site" /></td></tr>
         <tr><td>Votre adresse mail *: </td><td><input type="text" name="mail" required="required"/></td></tr>
         <tr><td>Votre numero de telephone *: </td><td><input type="text" name="telephone" required="required"/></td></tr>
-        <tr><td>Votre adresse *: </td><td><input type="text" name="adresse" required="required"/></td></tr>
+        <tr><td>Votre adresse (uniquement pour association): </td><td><input type="text" name="adresse" required="required"/></td></tr>
         <tr><td>Votre mot de passe *: </td><td><input type="text" name="mdp" required="required"/></td></tr></table>
 
         <p><input type="submit" value="Creer un compte"></p>
