89ac899a8dc147ba8c634b32a6c10b61797240cc
Rezader74 <57535481+Rezader74@users.noreply.github.com>
2019-12-10 11:03:20 +0100

    Page home

--- view/page/home.php
+++ view/page/home.php
@@ -1,1 +1,10 @@
-home
+<h3>Nos Objectifs:</h3>
+<p id='prem'>Notre site a pour but de repérer et de sauver les animaux abandonner ou perdu.
+	Pour réaliser ce projet nous avons besoin de toute la cummunauté souhaitant aider les annimaux.
+</p>
+<h3>Fonctionnement</h3>
+<p>Pour aider les animaux nous travaillons avec des associations animalières qui sont capable de s'occuper de tout ces animaux mais aussi des citoyen, en effet notre site permet aux particuliers de signaler un animal abandonner selon ces possiblilité.
+	Si cette dernière n'est pas dans la possiblité de garder l'animal, alors un signalement sera référencer sur le site afin de prévenir les association ou les membres a proximité si ils ont la possibilité de venir le chercher.
+</p>
+<h3>Utilité</h3>
+<p>Ce site permet donc de donner une meilleur vie a ces animaux dans un nouveaux foyer mais il permet aussi de retrouver un animal perdu, en effet si vous avez perdu votre animal car il c'est enfuit, vous pouvez savoir rapidement si il a été apreçu dans votre cartier.</p> 
